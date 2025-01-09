e04d7b39e9d76c1ec8921fcc283330338a5be71f
Sarvesh <sarveshletscode4u@gmail.com>
2025-01-09 21:47:25 +0530

    basics of js

---
+++ 01_basics.js
@@ -0,0 +1,36 @@
+//  Operations
+
+let str1 = "sarvesh";
+let str2 = " mishra";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("2" + 1);
+console.log(1 + 1 + "2");
+console.log("1" + 1 + 2);
+
+//Conversion
+
+let age = 22;
+let value = String(age);
+console.log(age);
+console.log(value);
+console.log(typeof value);
+
+let accountId = "1ew23ai";
+console.log(typeof accountId);
+let newId = Number(accountId);
+console.log(newId);
+console.log(typeof newId);
+
+// comparisons
+
+console.log(2 < 2);
+
+console.log(null == 0);
+console.log(null < 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+console.log(undefined > null);
+console.log(undefined == 0);
