39bbf22a433c43668306e0f55d9cf4cf68f9fbe4
Sarvesh <sarveshletscode4u@gmail.com>
2025-03-08 21:29:43 +0530

    basics of document object model

---
+++ 08_DOM in js/01_dom.js
@@ -0,0 +1,25 @@
+// Methods for getting element
+document.getElementById("title");
+const title = document.getElementById("title");
+title.getAttribute("class"); // retruns the specificd attrigbute as class or spam
+title.setAttribute("class", "main"); // used to set new attribute value
+
+// above method will overwrite the attribute class with new value main
+
+/*===================================*/
+
+// styling in js
+
+title.style.backgroundColor = "orange";
+
+// getting inner tags and texts
+
+const innerHtml = title.innerHTML;
+const innerText = title.innerText;
+
+console.log(innerHtml); // it will tags and text in the title tag
+console.log(innerText);  // it will only show the text in the tag
+
+// inner text can be manipulated by assining new value
+// title.innerText="hello world"    
+
